86b4ea458a522fb7306078277ce0ee9713992441
AndrewTaoWen <andrewwen6@gmail.com>
2023-12-18 15:46:13 -0500

    Initial Commit

---
+++ AITextToImage/AITextToImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AITextToImageApp.swift
+//  AITextToImage
+//
+//  Created by Andrew Wen on 2023-12-18.
+//
+
+import SwiftUI
+
+@main
+struct AITextToImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
